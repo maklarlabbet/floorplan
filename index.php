6f39ffbbb1dd0cc02b3501bfc9fad77f38401bc8
Maklarlabbet <info@maklarlabbet.se>
2026-07-14 21:07:29 +0530

    version 0.2.3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <link href="css/style.css" rel="stylesheet" />
 <?php
 echo '<h1>Welcome to the coding world!</h1>';
-echo '<p>Here you will learn exciting things..</p>';
+echo '<p>Here you will learn exciting things ahead.</p>';
 ?>
