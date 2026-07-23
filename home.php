245d3cd0e5bb9fe5dfd6dab96ae943c4fe65b3c1
Maklarlabbet <info@maklarlabbet.se>
2026-07-23 21:18:14 +0530

    Revert "Add new test content to home.php"
    This reverts commit a13bac73e944fa23bd08af9453b79cbe9dd15271.

--- home.php
+++ home.php
@@ -1,4 +1,3 @@
 <h1>Welcome to the floorplan!</h1>
 <p>We're on it and you'll be able to use it soon.</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-<p>This is new test content.</p>
