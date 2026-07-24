45aa9fe0faad5f5d95486927b010bafe2613925a
Maklarlabbet <info@maklarlabbet.se>
2026-07-24 20:06:09 +0530

    Remove all files from main branch
    Clearing out prior repo content on main; the active app now lives on master.

--- home.php
+++
@@ -1,3 +0,0 @@
-<h1>Welcome to the floorplan!</h1>
-<p>We're on it and you'll be able to use it soon.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
